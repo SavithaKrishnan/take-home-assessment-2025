f7f349d9dbeb226c717db36baf55c40cc414e7d0
Savi Krishnan <sk1700@georgetown.edu>
2025-06-25 23:19:09 -0400

    Update database.py
    remove creds

--- src/app/api/new_api/database.py
+++ src/app/api/new_api/database.py
@@ -3,8 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 #change PSQL credentials as needed
-DATABASE_URL = "postgresql://postgres:Choobertelephants2024@localhost:5432/state_registration_deadlines";
-
+DATABASE_URL = "postgresql://postgres:test@localhost:5432/state_registration_deadlines";
 
 engine = create_engine(DATABASE_URL)
 
